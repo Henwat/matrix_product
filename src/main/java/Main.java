b19185fabb5860f3d654978e8735369bbc11210c
Henrik Wathne <Wathneh@gmail.com>
2025-03-17 17:16:32 +0100

    Added some test cases to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,16 +15,35 @@ public class Main {
         int[][] grid = GridParser.parse(gridPath);
 
         List<GridProduct> product = GridSolver.solve(grid, kConsecutiveNumbers);
-        System.out.println("The largest product of the example matrix is:");
+        System.out.println("\nThe largest product of the example matrix is:");
         product.forEach(System.out::println); //Product: 93168306
 
-        //Solving the task
-        Path gridPath2 = Paths.get("src/main/resources/grid.txt");
+        //Test Case 2
+        Path gridPath2 = Paths.get("src/test/resources/test2.txt");
         int kConsecutiveNumbers2 = 4;
         int[][] grid2 = GridParser.parse(gridPath2);
 
         List<GridProduct> product2 = GridSolver.solve(grid2, kConsecutiveNumbers2);
+        System.out.println("\nThe largest product of the example matrix is:");
+        product2.forEach(System.out::println);
+
+        //Test Case 3
+        Path gridPath3 = Paths.get("src/test/resources/test3.txt");
+        int kConsecutiveNumbers3 = 4;
+        int[][] grid3 = GridParser.parse(gridPath3);
+
+        List<GridProduct> product3 = GridSolver.solve(grid3, kConsecutiveNumbers3);
+        System.out.println("\nThe largest product of the example matrix is:");
+        product3.forEach(System.out::println);
+
+
+        //Solving the task
+        Path gridPath4 = Paths.get("src/main/resources/grid.txt");
+        int kConsecutiveNumbers4 = 4;
+        int[][] grid4 = GridParser.parse(gridPath2);
+
+        List<GridProduct> product4 = GridSolver.solve(grid4, kConsecutiveNumbers4);
         System.out.println("\nThe largest product of the puzzle-matrix is:");
-        product2.forEach(System.out::println); //
+        product4.forEach(System.out::println); //
     }
 }
